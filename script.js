59586e51ae7c4dea055d08ad246526f0dbe311cd
Tanny2709 <123792042+Tanny2709@users.noreply.github.com>
2025-09-17 12:12:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+  let result = [];
+  let current = [];
+  let sum = 0;
+
+  for (let num of arr) {
+    // If adding this number exceeds n, start a new subarray
+    if (sum + num > n) {
+      result.push(current);
+      current = [];
+      sum = 0;
+    }
+    current.push(num);
+    sum += num;
+  }
+
+  // Push the last group if not empty
+  if (current.length > 0) {
+    result.push(current);
+  }
+
+  return result;
 };
 
-const n = prompt("Enter n: ");
+const n = parseInt(prompt("Enter n: "), 10);
 alert(JSON.stringify(divide(arr, n)));
